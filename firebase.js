57d41b3afc43ae9894945250704a8eca69e18fbc
NAV VARDHAN SINGH RANAWAT <25dec041@lnmiit.ac.in>
2026-01-01 21:43:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // 👈 Ye line miss ho gayi thi
+import { getFirestore } from "firebase/firestore"; 
 
-// ⚠️ Yahan apni ASLI Firebase Config wapas paste karo ⚠️
+// real api 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtsj6ZBuhBtoHBwGIi5ymJo6NT_KHA6Xs", // <--- Apni asli key yahan daalo
+  apiKey: "AIzaSyBtsj6ZBuhBtoHBwGIi5ymJo6NT_KHA6Xs", 
   authDomain: "lnm-verse.firebaseapp.com",
   projectId: "lnm-verse",
   storageBucket: "lnm-verse.appspot.com",
@@ -18,6 +18,7 @@ const app = initializeApp(firebaseConfig);
 // Exports
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app); // 👈 Ye line 'db' error ko fix karegi
+export const db = getFirestore(app); // 👈 for db error fix 
+
 
 export default app;
